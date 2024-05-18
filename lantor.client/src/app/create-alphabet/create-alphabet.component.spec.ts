b9c15a2578d7fa7b706607f825aa58fc8808047a
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2024-05-18 21:31:05 +0200

    add some tests to create-alphabet component

--- lantor.client/src/app/create-alphabet/create-alphabet.component.spec.ts
+++ lantor.client/src/app/create-alphabet/create-alphabet.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -12,11 +12,11 @@ describe('CreateAlphabetComponent', () => {
     const modalSpy = jasmine.createSpyObj<NgbActiveModal>('NgbActiveModal', ['close', 'dismiss']);
 
     await TestBed.configureTestingModule({
-      declarations: [ CreateAlphabetComponent ],
+      declarations: [CreateAlphabetComponent],
       imports: [FormsModule],
       providers: [{ provide: NgbActiveModal, useValue: modalSpy }]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CreateAlphabetComponent);
     component = fixture.componentInstance;
@@ -25,5 +25,86 @@ describe('CreateAlphabetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('maintains adjusted dimension %32', () => {
+    const dimInput = fixture.nativeElement.querySelector('#dimension');
+    const adjustedDim = fixture.nativeElement.querySelector('#adjusted-dimension');
+
+    dimInput.value = '10';
+    dimInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(adjustedDim.textContent).toBe('Adjusted dimension: 32');
+
+    dimInput.value = '33';
+    dimInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(adjustedDim.textContent).toBe('Adjusted dimension: 64');
+
+    dimInput.value = '32';
+    dimInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(adjustedDim.textContent).toBe('Adjusted dimension: 32');
+
+    dimInput.value = '100';
+    dimInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(adjustedDim.textContent).toBe('Adjusted dimension: 128');
+
+  });
+
+  it('enables create-button when both name and dimension are provided', () => {
+    const createButton = fixture.nativeElement.querySelector('#create-button');
+    expect(createButton.disabled).toBeTruthy();
+    const nameInput = fixture.nativeElement.querySelector('#name');
+    const dimInput = fixture.nativeElement.querySelector('#dimension');
+    nameInput.value = 'fakename';
+    nameInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(createButton.disabled).toBeTruthy();
+    dimInput.value = '32';
+    dimInput.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(createButton.disabled).toBeFalsy();
+  });
+
+  it('does not show error at the beginning and create-button is disabled', () => {
+
+  });
+
+  it('shows error when name gets empty and create-button is disabled', () => {
+
+  });
+
+  it('hides error when name gets value back', () => {
+
+  });
+
+  it('shows error when dimension is not positive integer number and create-button is disabled', () => {
+
+  });
+
+  it('hides error when dimension gets valid value back', () => {
+
+  });
+
+  it('shows both errors when both name and dim are invalid and create-button is disabled', () => {
+
+  });
+
+  it('create-button is enabled when both name and dimenions are valid', () => {
+
+  });
+
+  it('closes when clicking create', () => {
+
+  });
+
+  it('dismisses when cancelled', () => {
+
+  });
+
+  it('dismisses when clicking X', () => {
+
   });
 });
