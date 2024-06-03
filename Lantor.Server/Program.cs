414c5b0f513a8f90aa3b417ad196b474a1953263
Gabor Vasko-Szedlar <gabor.vaskoszedlar@gmail.com>
2024-06-03 22:15:28 +0200

    Adjust logging

--- Lantor.Server/Program.cs
+++ Lantor.Server/Program.cs
@@ -22,6 +22,8 @@ namespace Lantor.Server
 
             // Add services to the container.
 
+            builder.Services.AddSerilog(lc => lc.ReadFrom.Configuration(builder.Configuration));
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
